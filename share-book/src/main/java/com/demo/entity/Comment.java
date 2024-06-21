4612dd591680523b226a76d1940f02bf4d77e777
wsn1023 <166669336+wsn1023@users.noreply.github.com>
2024-06-21 22:08:04 +0800

    资源实例

---
+++ share-book/src/main/java/com/demo/entity/Comment.java
@@ -0,0 +1,134 @@
+package com.demo.entity;
+
+import java.util.Date;
+
+/**
+ * 评论实体类
+ *
+ 
+ * @company
+ * 2021-01-10 15:14
+ */
+public class Comment {
+
+    /**
+     * 编号
+     */
+    private Integer id;
+    /**
+     * 评论人id
+     */
+    private Integer userId;
+    /**
+     * 评论人实体
+     */
+    private User user;
+    /**
+     * 被评论的文章id
+     */
+    private Integer articleId;
+    /**
+     * 文章实体
+     */
+    private Article article;
+    /**
+     * 内容
+     */
+    private String commentContent;
+    /**
+     * 评论时间
+     */
+    private Date date;
+    /**
+     * 资源作者id
+     */
+    private Integer articleAuthorId;
+    /**
+     * 资源作者实体
+     */
+    private User articleAuthor;
+    /**
+     * 状态 0表示未审核 1代表审核通过 2.代表审核不通过
+     */
+    private Integer state;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Integer articleId) {
+        this.articleId = articleId;
+    }
+
+    public String getCommentContent() {
+        return commentContent;
+    }
+
+    public void setCommentContent(String commentContent) {
+        this.commentContent = commentContent;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+
+    public Integer getArticleAuthorId() {
+        return articleAuthorId;
+    }
+
+    public void setArticleAuthorId(Integer articleAuthorId) {
+        this.articleAuthorId = articleAuthorId;
+    }
+
+    public User getArticleAuthor() {
+        return articleAuthor;
+    }
+
+    public void setArticleAuthor(User articleAuthor) {
+        this.articleAuthor = articleAuthor;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+}
