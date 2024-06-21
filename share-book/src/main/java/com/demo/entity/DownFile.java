1bdde16a0d2cd2a1498de87c1c01b3cfb220dece
wsn1023 <166669336+wsn1023@users.noreply.github.com>
2024-06-21 22:27:16 +0800

    shili

---
+++ share-book/src/main/java/com/demo/entity/DownFile.java
@@ -0,0 +1,64 @@
+package com.demo.entity;
+
+public class DownFile {
+    private Integer resourceId;
+
+    private String resourceName;
+
+    private String resourceType;
+
+    private String pushDate;
+
+    private String fileName;
+
+
+    private String filePath;
+
+    public Integer getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(Integer resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public void setResourceType(String resourceType) {
+        this.resourceType = resourceType;
+    }
+
+    public String getPushDate() {
+        return pushDate;
+    }
+
+    public void setPushDate(String pushDate) {
+        this.pushDate = pushDate;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
