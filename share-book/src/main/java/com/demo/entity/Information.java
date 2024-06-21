125b56645448f5e1f7a49ea12bd36f90dcbd28ef
LZH345 <2373565567@qq.com>
2024-06-21 22:07:36 +0800

    实体类

---
+++ share-book/src/main/java/com/demo/entity/Information.java
@@ -0,0 +1,74 @@
+package com.demo.entity;
+
+import java.util.Date;
+
+/**
+ * 消息实体类
+ *
+ 
+ * 
+ * 
+ */
+public class Information {
+
+    /**
+     * 编号
+     */
+    private Integer id;
+    /**
+     * 消息所属者id
+     */
+    private Integer userId;
+    /**
+     * 消息所属者实体
+     */
+    private User user;
+    /**
+     * 内容
+     */
+    private String content;
+    /**
+     * 创建时间
+     */
+    private Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
